6f16e30d39826915970aaab946d3fc0c3223a4d6
KenCorp <kennethwarui35@gmail.com>
2023-07-05 13:14:26 +0300

    completed HTML file

---
+++ index.js
@@ -0,0 +1,8 @@
+// waiting for the DOM to load
+document.addEventListener('DOMContentLoaded', function(){
+    const form = document.querySelector('form');
+    let  nationText = document.querySelector('#nation-text');
+    let  countryList = document.querySelector('#country-list');
+
+
+});
